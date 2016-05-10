fcf36ed0ed5652d209596934ed0a6d571ca08a0a
big-boris <b.g.demchenko@yandex.ru>
2016-05-10 18:46:08 +0300

    Оптимизированы img, bxSlider для просмотра товаров

---
+++ builds/dev/js/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+  $('#product1').bxSlider({
+    mode: 'fade',
+    pagerCustom: '.bx-pager',
+    autoStart: false,
+    controls: false
+  });
+  $('#product2').bxSlider({
+    mode: 'fade',
+    pagerCustom: '.bx-pager',
+    autoStart: false,
+    controls: false
+  });
+  $('#product4').bxSlider({
+    mode: 'fade',
+    pagerCustom: '.bx-pager',
+    autoStart: false,
+    controls: false
+  });
+  $('#product5').bxSlider({
+    mode: 'fade',
+    pagerCustom: '.bx-pager',
+    autoStart: false,
+    controls: false
+  });
+
+});
