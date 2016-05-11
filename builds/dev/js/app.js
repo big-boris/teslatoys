2d4f29bc38a7ae75458b79b7a6c8edd10e1cf25e
big-boris <b.g.demchenko@yandex.ru>
2016-05-11 13:59:49 +0300

    Реализован блок меню

--- builds/dev/js/app.js
+++ builds/dev/js/app.js
@@ -42,3 +42,10 @@ $('.js-submitForm').click(function(event) {
 });
 
 });
+
+function myScrollTo(o) {
+  var t = $("." + o).offset().top - 10;
+  $("html, body").animate({
+      scrollTop:t
+  }, 1500);
+};
