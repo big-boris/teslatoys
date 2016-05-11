454de469ec4f8e549228eae2df495e39255fbfc7
big-boris <b.g.demchenko@yandex.ru>
2016-05-11 03:41:04 +0300

    Добавлена модаль заявки и ее заготовка js плюс заготовка меню

--- builds/dev/js/app.js
+++ builds/dev/js/app.js
@@ -25,4 +25,18 @@ $(document).ready(function(){
     controls: false
   });
 
+// $('.js-submitForm').click(function(event) {
+//   var name_orderform = $("#name_orderform");
+//   var phone_orderform = $("#phone_orderform");
+//   if($(name_orderform).val() && $(phone_orderform).val()) {
+//     $('#modal_order_form').hide();
+//   } else {  $(name_orderform).addClass("uk-form-danger");
+//             $(phone_orderform).addClass("uk-form-danger");
+//         }
+//         setTimeout(function() {
+//           $(name_orderform).removeClass("uk-form-danger"),
+//           $(phone_orderform).removeClass("uk-form-danger")
+//         },3000);
+// });
+
 });
