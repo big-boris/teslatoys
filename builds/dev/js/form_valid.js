916f31268742a4158f97d2835cf8905c2516288e
big-boris <b.g.demchenko@yandex.ru>
2016-06-14 23:07:34 +0300

    форма яКасса: проверка полей с выделением только пустых

--- builds/dev/js/form_valid.js
+++ builds/dev/js/form_valid.js
@@ -16,24 +16,42 @@ $(document).ready(function(){
         }
     });
 
-    $('.form_yakassa').submit(function(event) {
-        var ya_form_summa = $("#ya_form_summa");
-        var ya_form_phone = $("#ya_form_phone");
-        var ya_form_name = $("#ya_form_name");
-        var ya_form_mail = $("#ya_form_mail");
-        if(!$(ya_form_summa).val() || !$(ya_form_phone).val() || !$(ya_form_name).val() || !$(ya_form_mail).val()) {
-            $(ya_form_summa).addClass("alert");
-            $(ya_form_phone).addClass("alert");
-            $(ya_form_name).addClass("alert");
-            $(ya_form_mail).addClass("alert");
-            setTimeout(function() {
-                $(ya_form_summa).removeClass("alert"),
-                $(ya_form_phone).removeClass("alert"),
-                $(ya_form_name).removeClass("alert"),
-                $(ya_form_mail).removeClass("alert");
-            },2000);
-            event.preventDefault();
-        }
+    //*** Oldovyj variant
+    // $('.form_yakassa').submit(function(event) {
+    //     var ya_form_summa = $("#ya_form_summa");
+    //     var ya_form_phone = $("#ya_form_phone");
+    //     var ya_form_name = $("#ya_form_name");
+    //     var ya_form_mail = $("#ya_form_mail");
+    //     if(!$(ya_form_summa).val() || !$(ya_form_phone).val() || !$(ya_form_name).val() || !$(ya_form_mail).val()) {
+    //         $(ya_form_summa).addClass("alert");
+    //         $(ya_form_phone).addClass("alert");
+    //         $(ya_form_name).addClass("alert");
+    //         $(ya_form_mail).addClass("alert");
+    //         setTimeout(function() {
+    //             $(ya_form_summa).removeClass("alert"),
+    //             $(ya_form_phone).removeClass("alert"),
+    //             $(ya_form_name).removeClass("alert"),
+    //             $(ya_form_mail).removeClass("alert");
+    //         },2000);
+    //         event.preventDefault();
+    //     }
+    // });
+
+    $('.js-form_yakassa').submit(function(event) {
+        var field = new Array("sum", "cps_phone", "custName", "custEmail");
+        $(".js-form_yakassa").find(":input").each(function() {// проверяем каждое поле в форме
+            for(var i=0;i<field.length;i++){ // если поле присутствует в списке обязательных
+                if($(this).attr("name")==field[i]){ //проверяем поле формы на пустоту
+                    if(!$(this).val()){// если поле пустое
+                        $(this).addClass("alert");// устанавливаем рамку красного цвета
+                        event.preventDefault();
+                    }
+                    else{
+                        $(this).removeClass("alert");// устанавливаем рамку обычного цвета
+                    }
+                }
+            }
+        })
     });
 
 });
